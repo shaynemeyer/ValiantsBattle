9aadcfee63479c0bcde645bf4ff141a16e1ff092
shaynemeyer <shayne.meyer@gmail.com>
2015-12-18 11:00:54 -0800

    Finished initial layout.

--- ValiantsBattle/ViewController.swift
+++ ValiantsBattle/ViewController.swift
@@ -9,15 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var player: Player!
+    var enemy: Enemy!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
