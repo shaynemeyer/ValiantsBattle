75cac3b417d3bab85d766b11ccb202243bf214ad
shaynemeyer <shayne.meyer@gmail.com>
2015-12-18 10:13:19 -0800

    Added Character class.

---
+++ ValiantsBattle/Character.swift
@@ -0,0 +1,50 @@
+//
+//  Character.swift
+//  ValiantsBattle
+//
+//  Created by Shayne on 12/18/15.
+//  Copyright © 2015 Maynesoft LLC. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    
+    private var _hp: Int = 100
+    private var _attackPwr: Int = 10
+    
+    var attackPwr: Int {
+        get {
+            return _attackPwr
+        }
+    }
+    
+    var hp: Int {
+        get {
+            return _hp
+        }
+    }
+    
+    var isAlive: Bool {
+        get {
+            if hp <= 0 {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+    
+    init(startingHp: Int, attackPwr: Int) {
+        self._hp = startingHp
+        self._attackPwr = attackPwr
+    }
+    
+    func attemptAttack(attackPwr: Int) -> Bool {
+        self._hp -= attackPwr
+        
+        return true
+    }
+    
+    
+}
