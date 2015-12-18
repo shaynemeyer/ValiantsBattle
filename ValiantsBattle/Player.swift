172c10f49dac01c8f3be399483a0d5e8452df49c
shaynemeyer <shayne.meyer@gmail.com>
2015-12-18 10:14:40 -0800

    Added Player class.

---
+++ ValiantsBattle/Player.swift
@@ -0,0 +1,36 @@
+//
+//  Player.swift
+//  ValiantsBattle
+//
+//  Created by Shayne on 12/18/15.
+//  Copyright © 2015 Maynesoft LLC. All rights reserved.
+//
+
+import Foundation
+
+class Player: Character {
+    
+    private var _name = "Player"
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    private var _inventory = [String]()
+    
+    var inventory: [String] {
+        return _inventory
+    }
+    
+    func addItemToInventory(item: String) {
+        _inventory.append(item)
+    }
+    
+    convenience init(name: String, hp: Int, attackPwr: Int) {
+        self.init(startingHp: hp, attackPwr: attackPwr)
+        
+        _name = name
+    }
+}
