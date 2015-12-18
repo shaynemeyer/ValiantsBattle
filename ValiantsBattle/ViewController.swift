ebb7d15e986f813c4349c3c763de7c468052082e
shaynemeyer <shayne.meyer@gmail.com>
2015-12-18 12:53:44 -0800

    added gameOver and gameStart methods.

--- ValiantsBattle/ViewController.swift
+++ ValiantsBattle/ViewController.swift
@@ -13,9 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var printLbl: UILabel!
     @IBOutlet weak var enemyHpLbl: UILabel!
     @IBOutlet weak var playerHpLbl: UILabel!
+    @IBOutlet weak var enemyImg: UIImageView!
+    @IBOutlet weak var playerImg: UIImageView!
+    @IBOutlet weak var startOverBtn: UIButton!
     
     @IBOutlet weak var enemyAttackBtn: UIButton!
+    @IBOutlet weak var enemyAttackBtnLbl: UILabel!
     @IBOutlet weak var playerAttackBtn: UIButton!
+    @IBOutlet weak var playerAttackBtnLbl: UILabel!
     
     
     var player: Player!
@@ -36,7 +41,22 @@ class ViewController: UIViewController {
         self.playerAttackBtn.enabled = false
         
         // wait 3 seconds then re-enable
-        NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "disablePlayerAttackButton", userInfo: nil, repeats: false)
+        NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "enablePlayerAttackButton", userInfo: nil, repeats: false)
+        
+        if enemy.attemptAttack(player.attackPwr) {
+            printLbl.text = "Attacked \(enemy.name) for \(player.attackPwr) HP"
+            enemyHpLbl.text = "\(enemy.hp) HP"
+        } else {
+            printLbl.text = "Attack was unsuccessful!"
+        }
+        
+        if !enemy.isAlive {
+            enemyHpLbl.text = ""
+            printLbl.text = "\(player.name) Wins!!!"
+            enemyImg.hidden = true
+            
+            gameOver()
+        }
 
     }
     
@@ -46,16 +66,75 @@ class ViewController: UIViewController {
         self.enemyAttackBtn.enabled = false
         
         // wait 3 seconds then re-enable
-        NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "disableEnemyAttackButton", userInfo: nil, repeats: false)
+        NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "enableEnemyAttackButton", userInfo: nil, repeats: false)
+        
+        if player.attemptAttack(enemy.attackPwr) {
+            printLbl.text = "Attacked \(player.name) for \(enemy.attackPwr) HP"
+            playerHpLbl.text = "\(player.hp) HP"
+        } else {
+            printLbl.text = "Attack was unsuccessful!"
+        }
+        
+        if !player.isAlive {
+            playerHpLbl.text = ""
+            printLbl.text = "\(enemy.name) Wins!!"
+            playerImg.hidden = true
+            
+            gameOver()
+        }
+        
+        
 
     }
     
-    func disableEnemyAttackButton() {
+    
+    @IBAction func startOverBtnTapped(sender: AnyObject) {
+        gameStart()
+    }
+    
+    func enableEnemyAttackButton() {
         self.enemyAttackBtn.enabled = true
     }
     
-    func disablePlayerAttackButton() {
+    func enablePlayerAttackButton() {
         self.playerAttackBtn.enabled = true
+    }
+    
+    func gameOver() {
+        self.startOverBtn.hidden = false
+        
+        
+        
+        // hide attack buttons
+        self.enemyAttackBtn.hidden = true
+        self.enemyAttackBtnLbl.hidden = true
+        self.playerAttackBtn.hidden = true
+        self.playerAttackBtnLbl.hidden = true
+        // show players
+        self.playerImg.hidden = true
+        self.playerHpLbl.hidden = true
+        self.enemyImg.hidden = true
+        self.enemyHpLbl.hidden = true
+    }
+    
+    func gameStart() {
+        // hide start over button
+        self.startOverBtn.hidden = true
+        
+        // reset message.
+        self.printLbl.text = "Welcome to Valiants Battle."
+        
+        // show attack buttons
+        self.enemyAttackBtn.hidden = false
+        self.enemyAttackBtnLbl.hidden = false
+        self.playerAttackBtn.hidden = false
+        self.playerAttackBtnLbl.hidden = false
+        // show players
+        self.playerImg.hidden = false
+        self.playerHpLbl.hidden = false
+        self.enemyImg.hidden = false
+        self.enemyHpLbl.hidden = false
+        
     }
 }
 
